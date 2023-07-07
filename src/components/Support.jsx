ac3bcaf5215b8f8c7d0b620ae4303631dc2c07e9
I Wayan Bayu Arya <bayiabayu@gmail.com>
2023-07-08 01:45:45 +0700

    Created content in Support pages

---
+++ src/components/Support.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Wrapper from "../assets/wrappers/Support";
+
+export const Support = () => {
+  return (
+    <Wrapper className="container">
+      <div>
+        <h1>Senang untuk membantu Anda</h1>
+        <p></p>
+        <ul>
+          <b>Hubungi Kami:</b>
+          <li>
+            Jangan ragu untuk menghubungi kami jika Anda memiliki pertanyaan
+            atau membutuhkan bantuan. Kami siap membantu Anda!
+          </li>
+          <li>
+            Kami senang mendengar dari Anda! Silakan hubungi kami jika Anda
+            memiliki pertanyaan, masukan, atau masalah yang perlu diselesaikan.
+          </li>
+          <li>
+            Tim dukungan kami siap membantu Anda. Silakan jangan sungkan untuk
+            menghubungi kami melalui saluran komunikasi di bawah ini.
+          </li>
+        </ul>
+        <ul>
+          <b>Temukan Bantuan yang Anda Butuhkan:</b>
+          <li>
+            Kami menyediakan berbagai sumber daya untuk membantu Anda. Temukan
+            solusi untuk pertanyaan Anda di bawah ini atau hubungi kami untuk
+            bantuan lebih lanjut.
+          </li>
+          <li>
+            Cari jawaban untuk pertanyaan umum Anda dalam sumber daya dukungan
+            kami. Jika Anda tidak menemukan apa yang Anda cari, tim kami siap
+            membantu.
+          </li>
+          <li>
+            Kami telah menyusun informasi dan sumber daya penting untuk membantu
+            Anda. Temukan panduan pengguna, pertanyaan umum, dan kontak kami di
+            bawah ini.
+          </li>
+        </ul>
+        <ul>
+          <b>Tim Dukungan Kami:</b>
+          <li>
+            Kami memiliki tim dukungan yang berdedikasi dan siap membantu Anda
+            dengan setiap masalah yang Anda hadapi. Jangan ragu untuk
+            menghubungi kami!
+          </li>
+          <li>
+            Kami bangga dengan tim dukungan kami yang berpengalaman dan
+            responsif. Silakan hubungi kami jika Anda memerlukan bantuan apa
+            pun.
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="bayiabayu@gmail.com">Bayiabayu@gmail.com</a>
+          </li>
+          <li>082253381937</li>
+        </ul>
+      </div>
+    </Wrapper>
+  );
+};
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import main from '../assets/images/main.svg';
-import Wrapper from '../assets/wrappers/LandingPage';
-import { Logo } from '../components';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { RiLoginBoxLine } from 'react-icons/ri';
+import React from "react";
+import { Link } from "react-router-dom";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RiLoginBoxLine } from "react-icons/ri";
 import { AboutUs } from "../components/AboutUs";
+import { Support } from "../components/Support";
+import { MdLibraryAdd } from "react-icons/md";
+import { BsFileBarGraphFill } from "react-icons/bs";
 
 const Landing = () => {
   return (
@@ -74,7 +77,8 @@ const Landing = () => {
           </div>
         </div>
       </div>
-      <AboutUs/>
+      <AboutUs />
+      <Support />
     </Wrapper>
   );
 };
